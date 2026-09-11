0c9d00f72d2ddb9b68927a13228a1129851dbf95
Nicolas Gauvin <gauvin-arriaga@outlook.com>
2026-09-10 23:15:04 -0400

    created html file

--- csce242/Exercises/04js/scripts/intro.js
+++ csce242/Exercises/04js/scripts/intro.js
@@ -1,6 +1,4 @@
-function showMessage() {
-    console.log("I did it! The button was clicked!");
-}
-
-const btnClick = document.getElementById("btn-show-message");
-btnClick.onclick = showMessage();
+document.getElementById("btn-show-message").onclick = (e) => {
+    document.getElementById("p-message").innerHTML = "Hello World";
+    e.target.innerHTML = "done!";
+};
